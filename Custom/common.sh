99e6b5e74f63909928469a5dc00d90e6ab2493bc
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-03-20 01:34:02 +0800

    Update common.sh

--- Custom/common.sh
+++ Custom/common.sh
@@ -168,14 +168,6 @@ if [[ `grep -c "CONFIG_PACKAGE_luci-app-autopoweroff=y" ${Home}/.config` -eq '1'
 	
 fi
 
-if [[ `grep -c "CONFIG_PACKAGE_luci-theme-argon=y" ${Home}/.config` -eq '1' ]]; then
-	if [[ `grep -c "CONFIG_PACKAGE_luci-theme-argon_new=y" ${Home}/.config` -eq '1' ]]; then
-		sed -i 's/CONFIG_PACKAGE_luci-theme-argon_new=y/# CONFIG_PACKAGE_luci-theme-argon_new is not set/g' ${Home}/.config
-		echo " 您同时选择luci-theme-argon和luci-theme-argon_new，插件有冲突，已删除luci-theme-argon_new" >>CHONGTU
-		echo "插件冲突信息" > ${Home}/Chajianlibiao
-	fi
-fi
-
 if [[ `grep -c "CONFIG_TARGET_ROOTFS_EXT4FS=y" .config` -eq '1' ]]; then
 	echo " " > ${Home}/EXT4
 	echo " 请注意，您选择了ext4安装的固件格式" >> ${Home}/EXT4
