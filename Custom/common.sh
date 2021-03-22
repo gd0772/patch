82be1317814e98ef37e8e5f762b177da80c63d08
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-03-22 17:48:46 +0800

    Update common.sh

--- Custom/common.sh
+++ Custom/common.sh
@@ -7,8 +7,8 @@
 Diy_all() {
 git clone https://github.com/gd0772/package package/diy
 if [[ ${REGULAR_UPDATE} == "true" ]]; then
-git clone https://github.com/281677160/luci-app-autoupdate package/luci-app-autoupdate
-sed -i 's/"定时更新"/"更新固件"/g' package/luci-app-autoupdate/po/zh-cn/autoupdate.po
+git clone https://github.com/Hyy2001X/luci-app-autoupdate package/diy/luci-app-autoupdate
+sed -i 's/"定时更新"/"更新固件"/g' package/diy/luci-app-autoupdate/po/zh-cn/autoupdate.po
 mv "${PATH1}"/{AutoUpdate.sh,AutoBuild_Tools.sh} package/base-files/files/bin
 chmod -R +x package/base-files/files/bin
 fi
