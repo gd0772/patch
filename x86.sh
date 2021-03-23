84502fd15f195a3225ede1bc4cc99386a97e685e
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-03-24 07:19:37 +0800

    Update x86.sh

--- x86.sh
+++ x86.sh
@@ -82,6 +82,7 @@ sed -i 's/挂载 SMB 网络共享/挂载共享/g' package/lean/luci-app-cifs-mou
 sed -i 's/"文件浏览器"/"文件浏览"/g' package/diy/luci-app-filebrowser/po/zh-cn/filebrowser.po
 sed -i 's/msgstr "FTP 服务器"/msgstr "FTP 服务"/g' package/lean/luci-app-vsftpd/po/zh-cn/vsftpd.po
 sed -i 's/Rclone/网盘挂载/g' package/lean/luci-app-rclone/luasrc/controller/rclone.lua
+sed -i 's/"msgstr "Aria2""/"msgstr "Aria2下载""/g' feeds/luci/applications/luci-app-aria2/po/zh-cn/aria2.po
 sed -i 's/BaiduPCS Web/百毒网盘/g' package/lean/luci-app-baidupcs-web/luasrc/controller/baidupcs-web.lua
 sed -i 's/IPSec VPN 服务器/IPSec 服务/g' package/lean/luci-app-ipsec-vpnd/po/zh-cn/ipsec.po
 sed -i 's/V2ray 服务器/V2ray 服务/g' package/lean/luci-app-v2ray-server/po/zh-cn/v2ray_server.po
