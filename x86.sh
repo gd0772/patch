4a76eb412cc96b43109f74a7a6ffe07556b2f809
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-07-28 17:44:01 +0800

    Update x86.sh

--- x86.sh
+++ x86.sh
@@ -11,10 +11,8 @@ rm -rf ./feeds/luci/themes/luci-theme-material
 # echo '删除重复插件'
 rm -rf ./feeds/packages/net/smartdns
 rm -rf ./feeds/packages/admin/netdata
-rm -rf ./package/gd772/luci-lib-docker
 rm -rf ./package/lean/luci-app-netdata
 rm -rf ./package/lean/luci-app-jd-dailybonus
-rm -rf ./package/gd772/luci-app-pptp-server
 rm -rf ./feeds/luci/applications/luci-app-rp-pppoe-server
 
 # echo '修改 默认IP'
