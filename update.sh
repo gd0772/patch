d187ccfa2c676a10be67d91608f7afdd5df9a456
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-09-23 05:40:03 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -25,24 +25,23 @@ TIME() {
 	read -p " 输入您的选择： " CHOOSE
 	case $CHOOSE in
 	1)
+	TIME y "您选择了 [1] 更新至 2021.09.21 编译的 5.4.145 版本"
         bash <(curl -s -S -L https://git.io/phicomm_n1_5.4.y.sh)
-	TIME y "[1] 在线更新 至 2021.09.21编译的 5.4.145 版本"
 	break
 	;;
 	2)
+	TIME y "您选择了 [2] 更新至 2021.09.21 编译的 5.14.6 版本"
         bash <(curl -s -S -L https://git.io/phicomm_n1_5.4.y.sh)
-	TIME y "[2] 在线更新 至 2021.09.21编译的 5.14.6 版本"
 	break
 	;;
 	3)
-	exit 0
 	TIME r "您选择了 退出本次更新"
 	exit 0
-		break
-    		;;
-    		*)
-			TIME r "警告：输入错误,请输入正确的编号!"
-		;;
+	break
+    	;;
+    	*)
+	TIME r "警告：输入错误,请输入正确的编号!"
+	;;
 	esac
 	done
 }
