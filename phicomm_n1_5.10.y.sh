235df23969310d65c62d17f7f017f38cd54c5619
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-09-23 17:31:40 +0800

    Update phicomm_n1_5.10.y.sh

--- phicomm_n1_5.10.y.sh
+++ phicomm_n1_5.10.y.sh
@@ -18,13 +18,13 @@ TIME() {
 }
 cd /mnt/mmcblk2p4
 rm -rf *.sh Armbian_*
-url=https://git.io/n1_5.10.61.img
-Firmware=Armbian_Phicomm-N1-R21.8.6_5.10.61.tar.gz
-img=Armbian_Phicomm-N1-R21.8.6_5.10.61.img
+url=https://github.com/gd0772/AutoBuild-OpenWrt/releases/download/AutoUpdate/
+Firmware=Armbian_Phicomm-N1_OP-R21.9.18_5.14.6.img.tar.gz
+img=Armbian_Phicomm-N1_OP-R21.9.18_5.14.6.img
 echo -e "===========================================下载固件中(需科学上网,否则无法更新)================================================="
-wget $url && mv n1_5.10.61.img $Firmware
+curl $url/$Firmware
 TIME g "=====================================================下载完成,解压中========================================================="
 tar -zxvf *tar.gz && rm -f *.tar.gz
 TIME r "=====================================================解压完成,开始升级固件===================================================="
 chmod 755 update.sh
-bash update.sh Armbian_Phicomm-N1-R21.8.6_5.10.61.img
+bash update.sh $img
