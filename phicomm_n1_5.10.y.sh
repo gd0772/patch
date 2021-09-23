4fed2db369e8f00133ae2c78bac205895ad2370d
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-09-23 17:52:43 +0800

    Update phicomm_n1_5.10.y.sh

--- phicomm_n1_5.10.y.sh
+++ phicomm_n1_5.10.y.sh
@@ -18,11 +18,11 @@ TIME() {
 }
 cd /mnt/mmcblk2p4
 rm -rf *.sh Armbian_*
-url=https://github.com/gd0772/AutoBuild-OpenWrt/releases/download/AutoUpdate
+url=https://github-releases.githubusercontent.com/347537054/83519c13-25d8-4967-8dc5-bfc3d26d7d7d?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20210923%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20210923T095043Z&X-Amz-Expires=300&X-Amz-Signature=4d3317b832f9d72f3c8b1bf128f39ae88d96588fb7956192665fed117a8571eb&X-Amz-SignedHeaders=host&actor_id=0&key_id=0&repo_id=347537054&response-content-disposition=attachment%3B%20filename%3DArmbian_Phicomm-N1_OP-R21.9.18_5.14.6.img.tar.gz&response-content-type=application%2Foctet-stream
 Firmware=Armbian_Phicomm-N1_OP-R21.9.18_5.14.6.img.tar.gz
 img=Armbian_Phicomm-N1_OP-R21.9.18_5.14.6.img
 TIME g "===========================================下载固件中(需科学上网,否则无法更新)================================================="
-wget $url/$Firmware
+wget $url
 TIME g "=====================================================下载完成,解压中========================================================="
 tar -zxvf *tar.gz && rm -f *.tar.gz
 TIME r "=====================================================解压完成,开始升级固件===================================================="
