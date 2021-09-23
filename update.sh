ae48d3e6cc88f05446d1aeff886a73daa084d9d8
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-09-23 14:09:17 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ TIME() {
 	r) export Color="\e[31;1m";;
 	g) export Color="\e[32;1m";;
 	b) export Color="\e[34;1m";;
-	y) export Color="\e[33;m";;
+	y) export Color="\e[33;0m";;
 	z) export Color="\e[35;1m";;
 	l) export Color="\e[36;1m";;
       esac
@@ -19,12 +19,13 @@ TIME() {
 }
 
 	while :; do
-	TIME g "--------------------------------------------------"
+	TIME g "---------------------------------------------------"
 	TIME g "[1] 更新至 2021.09.21 编译的 R21.9.18 5.4.145 版本"
 	echo
 	TIME g "[2] 更新至 2021.09.21 编译的 R21.9.18 5.14.6 版本"
 	echo
 	TIME g "[3] 说啥也不好使了 我要继续做钉子户 退出放弃本次更新"
+	TIME g "---------------------------------------------------"
 	read -p " 请输入您的选择： " CHOOSE
 	case $CHOOSE in
 	1)
