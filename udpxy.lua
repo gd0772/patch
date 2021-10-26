c42067e4f3901252d141e4084382aee753f27086
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-10-26 18:02:23 +0800

    Add files via upload

---
+++ udpxy.lua
@@ -0,0 +1,14 @@
+-- Copyright 2014 Álvaro Fernández Rojas <noltari@gmail.com>
+-- Licensed to the public under the Apache License 2.0.
+
+module("luci.controller.udpxy", package.seeall)
+
+function index()
+	if not nixio.fs.access("/etc/config/udpxy") then
+		return
+	end
+
+	local page = entry({"admin", "services", "udpxy"}, cbi("udpxy"), _("UDPxy组播"))
+	page.dependent = true
+
+end
