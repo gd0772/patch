90b65b01236459f6784b36dd40bfec0d33696ad4
gd0772 <61563545+gd0772@users.noreply.github.com>
2022-01-01 14:27:42 +0800

    Update gd772.sh

--- gd772.sh
+++ gd772.sh
@@ -235,9 +235,9 @@ sed -i 's/services/nas/g' package/lean/luci-app-aliyundrive-webdav/luasrc/model/
 sed -i 's/services/nas/g' package/lean/luci-app-aliyundrive-webdav/luasrc/view/aliyundrive-webdav/*.htm
 
 TIME l "菜单调理 完成"
-echo
-TIME y "更新固件 编译日期"
-sed -i "s/2021.10.01/$(TZ=UTC-8 date "+%Y.%m.%d") 元旦新年版/g" package/lean/autocore/files/x86/index.htm
+#echo
+#TIME y "更新固件 编译日期"
+#sed -i "s/2022.01.01/$(TZ=UTC-8 date "+%Y.%m.%d")/g" package/lean/autocore/files/x86/index.htm
 
 # echo 
 #TIME y "更换内核"
